62e37e1d9118699d13a7d58a62df311ea8ea1c3a
Javohir0411 <javohirabduhakimov979@gmail.com>
2024-02-01 03:23:06 +0500

    imtihon

--- javohir/models.py
+++ javohir/models.py
@@ -1,3 +1,33 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-class
+
+class AbstractModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Profile(AbstractModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=90, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(AbstractModel):
+    user = models.CharField(max_length=78)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
--- javohir/views.py
+++ javohir/views.py
@@ -1,3 +1,92 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+from .forms import UserRegistrationModelForm, UserLoginForm
+# from .models import Profile, Post
+
+
+class HomePageView(View):
+    def get(self, request):
+        return render(request, 'javohir/home.html')
+
+
+class UserRegisterView(View):
+    def get(self, request):
+        form = UserRegistrationModelForm()
+        return render(request, "javohir/register.html", {"form": form})
+
+    def post(self, request):
+        form = UserRegistrationModelForm(data=request.POST)
+        if form.is_valid():
+            messages.success(request, "User registered successfully")
+            form.save()
+            return redirect('javohir:login')
+        else:
+            return render(request, "javohir/register.html", {"form": form})
+
+
+class UserLoginView(View):
+    def get(self, request):
+        form = UserLoginForm()
+        return render(request, "javohir/login.html", {"form": form})
+
+    def post(self, request):
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print(request.COOKIES)
+                messages.success(request, "User logged in successfully")
+                return redirect("javohir:home-page")
+            else:
+                messages.error(request, "Username or password is wrong")
+                return redirect("javohir:login")
+        else:
+            return render(request, "javohir/login.html", {"form": form})
+
+
+class UserLogoutView(View):
+    def get(self, request):
+        return render(request, "javohir/logout.html")
+
+    def post(self, request):
+        logout(request)
+        messages.info(request, "User logged out successfully")
+        return redirect('javohir:home-page')
+
+
+class UserAboutView(View):
+    def get(self, request):
+        return render(request, "javohir/about.html")
+
+
+class UserHomeView(View):
+    def get(self, request):
+        return render(request, "javohir/home.html")
+
+
+class UserPostDetailView(View):
+    def get(self, request):
+        return render(request, "javohir/post_detail.html")
+
+
+class UserPostConfirmDeleteView(View):
+    def get(self, request):
+        return render(request, "javohir/post_confirm_delete.html")
+
+
+class UserPostsView(View):
+    def get(self, request):
+        return render(request, "javohir/user_posts.html")
+
+
+class UserPostsFormView(View):
+    def get(self, request):
+        return render(request, "javohir/post_form.html")
